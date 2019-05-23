7826aff4e44baa085683bc494ebc06f8860943e9
Michelle <you@example.com>
2019-05-23 15:43:27 +0800

    update

--- config/database.php
+++ config/database.php
@@ -93,12 +93,14 @@ return [
 
         'mongodb' => [
             'driver'   => 'mongodb',
+            'dsn'      => env('DB_DSN'),
             'host'     => env('DB_HOST', 'localhost'),
             'port'     => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
             'options'  => [
+                'ssl' => true,
                 // 'database' => 'admin' // sets the authentication database required by mongo 3
             ]
         ],
